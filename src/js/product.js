ad353aa3cf36ac1fa26d716f09e018224873f554
omarlinp <omarlinp@gmail.com>
2026-05-25 21:52:20 -0400

    completed the Team 2 assigment

--- src/js/product.js
+++ src/js/product.js
@@ -1,16 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import {GetParam} from "./utils.mjs";
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+const productId = GetParam("product");
+console.log(productId);
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+
