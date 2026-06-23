83de6778f2d1e1294509c40f0fcd72a2c4042d55
omarlinp <omarlinp@gmail.com>
2026-06-23 18:40:45 -0400

    created the files and added the list of objects

---
+++ src/js/product-list.js
@@ -0,0 +1,7 @@
+import productList from "./productList.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+
+loadHeaderFooter();
+const category = getParam("category")
+productList(".product-list", category);
